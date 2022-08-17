ad3603f985a2bcf62398a825e15b46a1926eaa26
Hannah <xhan104@gmail.com>
2022-08-17 04:03:05 +0100

    storybook in a storybook

---
+++ example-with-storybook/src/components/UserItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Avatar } from '@apricot13/trail-mix';
+
+const Container = styled.div`
+  background: #eee;
+  margin-bottom: 1em;
+  padding: 0.5em;
+`;
+
+const Name = styled.span`
+  color: #333;
+  font-size: 16px;
+`;
+
+export default ({ user: { name, avatarUrl } }) => (
+  <Container>
+    <Avatar username={name} src={avatarUrl} />
+    <Name>{name}</Name>
+  </Container>
+);
